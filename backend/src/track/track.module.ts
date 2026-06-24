ee9a1497b3e8f71c786f6ec974a4eb83d155188a
Apps Developers <apps@innovacloud.co>
2026-06-24 14:16:57 +0000

    Deleted backend

--- backend/src/track/track.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TrackController } from './track.controller';
-import { ComunicadosModule } from '../comunicados/comunicados.module';
-
-@Module({
-  imports: [ComunicadosModule],
-  controllers: [TrackController],
-})
-export class TrackModule {}
